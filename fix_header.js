220740c2623339643514435d5ff1d858f3ba8f45
Zakaria <zakariamohammedalisaif-beep@example.com>
2026-08-25 03:16:32 +0300

    Fix garbled Arabic characters in header due to previous script encoding

---
+++ fix_header.js
@@ -0,0 +1,6 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('pdf-template.html', 'utf8');
+// Use a regex that catches any sequence of replacement characters or mojibake inside the h2
+html = html.replace(/<h2[^>]*>.*?<\/h2>/, '<h2 style="font-family: \'Amiri\', \'Times New Roman\', serif; font-size: 26px; color: #000; margin: 0 0 5px 0; font-weight: bold; letter-spacing: 0px;">الْمَمْلَكَةُ الْعَرَبِيَّةُ السُّعُودِيَّةُ</h2>');
+fs.writeFileSync('pdf-template.html', html, 'utf8');
+console.log('Fixed header');
